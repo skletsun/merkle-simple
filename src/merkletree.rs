19be8fb5ace916b0d65fdda1c2e7d183349d1886
Sergey Kletsun <sergey.kletsun@gmail.com>
2017-07-03 14:50:18 +0300

    Covered case with empty initialization array

--- src/merkletree.rs
+++ src/merkletree.rs
@@ -33,51 +33,56 @@ impl <T> MerkleTree<T> where T: Hashable {
         self.root.hash()
     }
 
-    pub fn from_vector(data: Vec<T>) -> Self where T: Hashable {
-        // TODO Handle empty array case
-
+    pub fn from_vector(data: Vec<T>) -> Option<Self> where T: Hashable {
         let count = data.len();
-        let mut height = 0;
 
-        let mut src = VecDeque::with_capacity(count);
+        match count {
+            0 => None,
+            _ => {
+                let mut height = 0;
+                let mut src = VecDeque::with_capacity(count);
 
-        // create first layer of leaves
-        for val in data {
-            src.push_back(TreeElement::new_leaf(val));
-        }
-
-        // build tree without recursion, layer by layer
-        while src.len() > 1 {
-            let mut new_layer: VecDeque<TreeElement<T>> = VecDeque::with_capacity(src.len()/2);
-
-            while !src.is_empty() {
-                // check for the case when we have the one element only - it will be the Leaf
-                if src.len() == 1 {
-                    // It's a leaf - push it to the next turn of processing
-                    new_layer.push_back(src.pop_front().unwrap());
-                } else {
-                    // we take two elements and produce the Node
-                    let left_node = src.pop_front().unwrap();
-                    let right_node = src.pop_front().unwrap();
-                    let node = TreeElement::new_node(left_node, right_node);
-                    // and push it for further processing
-                    new_layer.push_back(node);
+                // create first layer of leaves
+                for val in data {
+                    src.push_back(TreeElement::new_leaf(val));
                 }
-            }
-            // we've just processed the new layer of our tree
-            // increase the height of tree
-            height += 1;
-            // pass our prepared queue to the next iteration if any
-            src = new_layer;
-        }
-        // we ended up with only one element - this is the root node
-        let root = src.pop_back().unwrap();
 
-        // return the resulting tree
-        MerkleTree {
-            root: root,
-            count: count,
-            height: height
+                // build tree without recursion, layer by layer
+                while src.len() > 1 {
+                    let mut new_layer: VecDeque<TreeElement<T>> = VecDeque::with_capacity(src.len()/2);
+
+                    while !src.is_empty() {
+                        // check for the case when we have the one element only - it will be the Leaf
+                        if src.len() == 1 {
+                            // It's a leaf - push it to the next turn of processing
+                            new_layer.push_back(src.pop_front().unwrap());
+                        } else {
+                            // we take two elements and produce the Node
+                            let left_node = src.pop_front().unwrap();
+                            let right_node = src.pop_front().unwrap();
+                            let node = TreeElement::new_node(left_node, right_node);
+                            // and push it for further processing
+                            new_layer.push_back(node);
+                        }
+                    }
+                    // we've just processed the new layer of our tree
+                    // increase the height of tree
+                    height += 1;
+                    // pass our prepared queue to the next iteration if any
+                    src = new_layer;
+                }
+                // we ended up with only one element - this is the root node
+                let root = src.pop_back().unwrap();
+
+                // return the resulting tree
+                Some(
+                    MerkleTree {
+                        root: root,
+                        count: count,
+                        height: height
+                    }
+                )
+            }
         }
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -9,7 +9,7 @@ fn produce_tree_from_one_elem() {
     let data = vec![format!("one")];
     let test_hash = Utils::hash_leaf_data(data[0].get_bytes());
 
-    let tree = MerkleTree::from_vector(data);
+    let tree = MerkleTree::from_vector(data).unwrap();
 
     assert_eq!(tree.count(), 1);
     assert!(!tree.is_empty());
@@ -22,7 +22,7 @@ fn produce_tree_from_two_elems() {
     let data = vec!["one".to_string(), "two".to_string()];
     let test_hash = Utils::hash_node_data(data[0].get_bytes(), data[1].get_bytes());
 
-    let tree = MerkleTree::from_vector(data);
+    let tree = MerkleTree::from_vector(data).unwrap();
 
     assert_eq!(tree.count(), 2);
     assert!(!tree.is_empty());
