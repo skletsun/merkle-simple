ce6c04cbd10d12a998ae71bf0743cf372d1186f0
Sergey Kletsun <sergey.kletsun@gmail.com>
2017-07-05 18:37:14 +0300

    Added test for failed inclusion proof

--- tests/proof.rs
+++ tests/proof.rs
@@ -7,11 +7,24 @@ fn test_inclusion_success() {
     let data = vec![format!("one"), format!("two"), format!("three"), format!("four")];
     let tree = MerkleTree::from_vector(data).unwrap();
 
-    let data_to_check = format!("one");
+    let data_to_check = format!("two");
 
     let proof = tree.get_proof(data_to_check);
     assert!(proof.is_some());
 
     let result = proof.unwrap().validate(tree.root_hash());
     assert!(result);
+}
+
+
+#[test]
+#[should_panic]
+fn test_inclusion_fail() {
+    let data = vec![format!("one"), format!("two"), format!("three"), format!("four")];
+    let tree = MerkleTree::from_vector(data).unwrap();
+
+    let data_to_check = format!("five");
+
+    let proof = tree.get_proof(data_to_check);
+    assert!(proof.is_none());
 }
