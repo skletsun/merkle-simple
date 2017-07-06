1331e4f1daaad67a4f3258f087a0ffe84e5a5267
Sergey Kletsun <sergey.kletsun@gmail.com>
2017-07-06 14:38:57 +0300

    Improved test

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,9 +1,10 @@
 #![feature(test)]
-
+#[allow(unused_imports)]
 extern crate merkle_simple;
 extern crate test;
 
 use test::Bencher;
+#[allow(unused_imports)]
 use merkle_simple::{MerkleTree, Hashable};
 
 #[bench]
@@ -22,9 +23,25 @@ fn big_tree_creation(b: &mut Bencher) {
         s.push('a');
     }
 
-    for _ in 1..10000 {
+    for _ in 1..1000 {
         input_data.push(s.clone());
     }
 
     b.iter(|| MerkleTree::from_vector(input_data.clone()));
+}
+
+#[bench]
+fn bench_proof_creation(b: &mut Bencher) {
+    let input_data = vec!["one".to_string(), "two".to_string(), "three".to_string(), "four".to_string(), "five".to_string(), "six".to_string(), "seven".to_string(), "eight".to_string()];
+    let tree = MerkleTree::from_vector(input_data.clone()).unwrap();
+
+    b.iter(|| tree.get_proof("eight".to_string()));
+}
+
+#[bench]
+fn bench_proof_validation(b: &mut Bencher) {
+    let input_data = vec!["one".to_string(), "two".to_string(), "three".to_string(), "four".to_string(), "five".to_string(), "six".to_string(), "seven".to_string(), "eight".to_string()];
+    let tree = MerkleTree::from_vector(input_data.clone()).unwrap();
+
+    b.iter(|| tree.get_proof("eight".to_string()).map(|proof| proof.validate(tree.root_hash())));
 }
--- tests/proof.rs
+++ tests/proof.rs
@@ -1,24 +1,23 @@
 extern crate merkle_simple;
 
+#[allow(unused_imports)]
 use merkle_simple::{MerkleTree, Hashable};
 
 #[test]
 fn test_inclusion_success() {
     let data = vec![format!("one"), format!("two"), format!("three"), format!("four")];
-    let tree = MerkleTree::from_vector(data).unwrap();
+    let tree = MerkleTree::from_vector(data.clone()).unwrap();
 
-    let data_to_check = format!("two");
+    for data_to_check in data {
+        let proof = tree.get_proof(data_to_check);
+        assert!(proof.is_some());
 
-    let proof = tree.get_proof(data_to_check);
-    assert!(proof.is_some());
-
-    let result = proof.unwrap().validate(tree.root_hash());
-    assert!(result);
+        let result = proof.unwrap().validate(tree.root_hash());
+        assert!(result);
+    }
 }
 
-
 #[test]
-#[should_panic]
 fn test_inclusion_fail() {
     let data = vec![format!("one"), format!("two"), format!("three"), format!("four")];
     let tree = MerkleTree::from_vector(data).unwrap();
